983ad786a2992f5174b528a56f2bae7214ab37c6
Nick Klauer <klauer@gmail.com>
2011-03-29 19:35:17 -0500

    add option parsing error checking

--- lib/tbox-template.rb
+++ lib/tbox-template.rb
@@ -6,20 +6,29 @@ module Tbox
     class Cli
 
       def run
-        opts = Slop.parse do
+        opts = Slop.new(:strict => true) do
           banner "Usage: tb [options]"
           on :c, :context, "Deploy to context [location]"
           on :a, :app, "Application Name [name]"
           on :s, :sinatra, "Use Sinatra?"
+          on :h, :help, "Show detailed Help"
         end
         unless ARGV.size > 0
           puts opts
+        else
+          begin
+            opts.parse
+          rescue Slop::InvalidOptionError => e
+            puts "\n#{e.message}\n\n"
+            puts opts
+            exit
+          end
         end
         create_tb_app(opts)
       end
 
       def create_tb_app(opts)
-      
+
       end
     end
   end
