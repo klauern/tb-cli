54ff5b09f2782e72d8e9a2ca9ef06e8ca9134620
Klauer - Nick Klauer <nicholasklauer@alliantenergy.com>
2011-04-13 19:23:42 -0500

    convert a bit to Thor

--- lib/tb-cli.rb
+++ lib/tb-cli.rb
@@ -1,6 +1,35 @@
+require 'rubygems'
 require 'thor'
 
+$: << File.expand_path(File.dirname(__FILE__) + "/../lib")
+require 'tb-cli/actions/new'
+
 class Tbox < Thor
+
+  default_task :show_banner
+
+  # Default task to show when no options are passed in.
+  desc "help", "Show this help message"
+  def show_banner
+    puts BANNER
+    help
+  end
+  
+  BANNER = <<-BAN
+Torquebox CLI  
+tb-cli [action] [options]     # See below for more details on actions
+
+Use this template generator to make quick Rack applications.  If you want a rails app, please use the 
+application template provided by Torquebox: http://torquebox.org/documentation/DEV/rails.html#using-the-rails-application-template
+
+rails new my_app -m $TORQUEBOX_HOME/share/rails/template.rb
+  
+  BAN
+
+end
+
+Tbox.start
+=begin
 
   def run
     opts = Slop.new(:strict => true) do
@@ -35,4 +64,4 @@ class Tbox < Thor
       puts "App name will be called #{opts[:app]}"
     end
   end
-end
+=end
---
+++ lib/tb-cli/actions/new.rb
@@ -0,0 +1,12 @@
+module Actions
+
+  class New < Thor
+
+    namespace :new
+
+    desc "rack", "Create a new Rack Application Template"
+    def rack
+    end
+
+  end
+end
